d0b2f9b67d2eaf058909e0261cda3410f738b8f5
Jeff Nelson <jtnelson1@gmail.com>
2014-09-23 07:04:52 -0400

    fix issues with help option

--- src/main/java/org/cinchapi/concourse/cli/Options.java
+++ src/main/java/org/cinchapi/concourse/cli/Options.java
@@ -61,6 +61,9 @@ public class Options {
         }
     }
 
+    @Parameter(names = { "-h", "--help" }, help = true, hidden = true)
+    public boolean help;
+
     @Parameter(names = { "-h", "--host" }, description = "The hostname where the Concourse Server is located")
     public String host = prefsHandler != null ? prefsHandler.getHost()
             : "localhost";
